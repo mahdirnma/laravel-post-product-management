4aa5533425769347b24f7a1505404387b516cf26
mahdirnma <rahnamamahdi78@gmail.com>
2025-09-14 17:03:46 +0330

    create register and login processes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::class, 'index'])->name('home');
-Route::get('/login', [AuthController::class, 'loginForm'])->name('login.form');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'registerForm'])->name('register.form');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('login.form');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/register', [AuthController::class, 'registerForm'])->name('register.form');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->name('home');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
